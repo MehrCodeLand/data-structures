28c243a89536e7df2433b640a46668ffe7f6d79a
MehrCodeLand <mehrcodeland@gmail.com>
2023-05-03 00:15:46 +0330

    update c sharp code

--- algorithm/CSharp/BinarySearch.cs
+++ algorithm/CSharp/BinarySearch.cs
@@ -1,21 +1,24 @@
 public int BinarySearch(int[] numbers , int left , int right , int number)
 {
-	// to fihnd number
-	int index = left + (right - left) / 2;
+	while(left <= right)
+	{
+		// to fihnd number
+		int index = left + (right - left) / 2;
 
+		// lucky day
+		if (numbers[index] == number)
+			return index;
 
-	// lucky day
-	if (numbers[index] == number)
-		return index;
+		// if number smaller
+		if (numbers[index] < number)
+			right = index - 1;
 
-	// if number smaller
-	if (numbers[index] < number)
-		right = index - 1;
+		// else number bigger
+		else
+			left = index + 1;
 
-	// else number bigger
-	else
-		left = index + 1;
-
+	}
+	
 	return -10;
 
 }
